9f3e75fc90ef20c5effc3717de8a2f297d63b865
Germain Salvato Vallverdu <germain.vallverdu@gmail.com>
2022-06-28 09:50:08 +0200

    add the __init__.py file in the app folder

---
+++ src/nba_stat_app/__init__.py
@@ -0,0 +1,5 @@
+# coding: utf-8
+
+""" this file make the nba_stat_app/ folder a python module. 
+This is needed to make the app.py file and components folder file to be 
+imported in the app or in the run_app.py file """ 
